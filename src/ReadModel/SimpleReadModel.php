667a7178c86d70f89ff61c863394698f0fd2ab9c
Timo Ebel <timo.ebel@gmail.com>
2018-02-20 23:36:09 +0100

    Added abstract SimpleReadModel with basic toArray() and initializeFromArray() functionality.

---
+++ src/ReadModel/SimpleReadModel.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) 2018 Timo Ebel
+ *
+ * This work is licensed under the terms of the MIT license.
+ * For a copy, see <https://opensource.org/licenses/MIT> or the LICENSE file included in this project.
+ */
+
+namespace Rekhyt\DDDBase\ReadModel;
+
+abstract class SimpleReadModel implements ReadModel
+{
+    public function toArray(): array
+    {
+        return get_object_vars($this);
+    }
+
+    public function initializeFromArray(array $array)
+    {
+        foreach ($array as $property => $value) {
+            if (!property_exists($this, $property)) {
+                continue;
+            }
+
+            $this->$property = $value;
+        }
+    }
+}
